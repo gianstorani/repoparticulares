5548185c54994ba30f6f103bb0e8f55b0a6f6826
gianfranco <gianstorani9@gmail.com>
2020-08-17 01:52:42 -0300

    Inicia html

--- mysite/settings.py
+++ mysite/settings.py
@@ -52,10 +52,11 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'mysite.urls'
 
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'sitio','templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
