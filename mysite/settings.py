fdada9528db39966ed951b0f9dc740b92d97d5be
gianfranco <gianstorani9@gmail.com>
2020-08-25 00:57:11 -0300

    Agregue login y logout

--- mysite/settings.py
+++ mysite/settings.py
@@ -121,3 +121,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+# Redirect to home URL after login (Default redirects to /accounts/profile/)
+LOGIN_REDIRECT_URL = '/'
